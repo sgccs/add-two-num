2ab7e3c599c5db60a3931d6526b251d6c173eb99
sgccs <josyulavenkata@iitbhilai.ac.in>
2022-11-28 21:54:05 +0530

    Revert "contradiction"
    This reverts commit 9796d58be50d01bb6dbf89021dedf510749b6f62.

--- add.py
+++ add.py
@@ -1,8 +1,6 @@
 import sys
 def input():return sys.stdin.readline().strip()
-def add(t):
-    while(t>0):
-        a,b=map(int,input().split())
-        print(a+b)
 t=int(input())
-add(t)
+while(t>0):
+    a,b=int(input())
+    print(a+b)
